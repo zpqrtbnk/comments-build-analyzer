b979d7c59d4ab758cc3e8c65074e9d83c114344f
Stephan <stephane.gay@hazelcast.com>
2021-03-15 18:55:52 +0100

    Initial import

---
+++ ZpqrtBnk.CommentsBuildAnalyzer/ZpqrtBnk.CommentsBuildAnalyzer.Test/ZpqrtBnkCommentsBuildAnalyzerUnitTests.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) 2008-2021, ZpqrtBnk. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VerifyCS = ZpqrtBnk.CommentsBuildAnalyzer.Test.CSharpCodeFixVerifier<
+    ZpqrtBnk.CommentsBuildAnalyzer.ZpqrtBnkCommentsBuildAnalyzerAnalyzer,
+    ZpqrtBnk.CommentsBuildAnalyzer.ZpqrtBnkCommentsBuildAnalyzerCodeFixProvider>;
+
+namespace ZpqrtBnk.CommentsBuildAnalyzer.Test
+{
+    [TestClass]
+    public class ZpqrtBnkCommentsBuildAnalyzerUnitTest
+    {
+        [TestMethod]
+        public async Task Test()
+        {
+            const string code = @"
+using System;
+
+// define namespace
+namespace NameSpace
+{
+    // define class
+    // FIXME: use an explicit name
+    public class Foo
+    {
+        // FIXME! this is really bad
+        public void Meh() { }
+    }
+}";
+
+            var expected1 = new DiagnosticResult(ZpqrtBnkCommentsBuildAnalyzerAnalyzer.DiagnosticId, 
+                    DiagnosticSeverity.Warning)
+                .WithMessage("FIXME comment in code.")
+                .WithLocation(8, 5);
+
+            var expected2 = new DiagnosticResult(ZpqrtBnkCommentsBuildAnalyzerAnalyzer.DiagnosticId, 
+                    DiagnosticSeverity.Error)
+                .WithMessage("FIXME! comment in code.")
+                .WithLocation(11, 9);
+
+            await VerifyCS.VerifyAnalyzerAsync(code, expected1, expected2);
+        }
+    }
+}
---
+++ ZpqrtBnk.CommentsBuildAnalyzer/ZpqrtBnk.CommentsBuildAnalyzer/ZpqrtBnkCommentsBuildAnalyzerAnalyzer.cs
@@ -0,0 +1,149 @@
+﻿// Copyright (c) 2008-2021, ZpqrtBnk. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
+
+namespace ZpqrtBnk.CommentsBuildAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ZpqrtBnkCommentsBuildAnalyzerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "ZB1001";
+
+        private const string Category = "ZpqrtBnk";
+        private const string HelpLinkUri = "https://github.com/zpqrtbnk/Zpqrtbnk.CommentsBuildAnalyzer";
+
+        private static readonly LocalizableString Title = "Comments Analyzer.";
+        //private static readonly LocalizableString MessageFormat = "Found a FIX*ME comment.";
+        //private static readonly LocalizableString Description = "Found a FIX*ME comment.";
+
+        private static readonly DiagnosticDescriptor WarnOnFixmeRule = new DiagnosticDescriptor(
+            DiagnosticId, 
+            Title, 
+            "FIXME comment in code.", 
+            Category, 
+            DiagnosticSeverity.Warning, // report as a warning
+            true, 
+            //description: Description,
+            helpLinkUri: HelpLinkUri);
+
+        private static readonly DiagnosticDescriptor ErrOnFixmeRule = new DiagnosticDescriptor(
+            DiagnosticId,
+            Title,
+            "FIXME! comment in code.",
+            Category,
+            DiagnosticSeverity.Error, // report as an error
+            true,
+            //description: Description,
+            helpLinkUri: HelpLinkUri);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics 
+            => ImmutableArray.Create(WarnOnFixmeRule, ErrOnFixmeRule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze|GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxTreeAction(HandleSyntaxTree);
+        }
+
+        private void HandleSyntaxTree(SyntaxTreeAnalysisContext context)
+        {
+            try
+            {
+                TryHandleSyntaxTree(context);
+            }
+            catch
+            {
+                // Analyzers should not throw
+                if (Debugger.IsAttached) throw;
+            }
+        }
+
+        private static readonly char[] TrimChars = { '/', '*', ' ' };
+
+        private static void TryHandleSyntaxTree(SyntaxTreeAnalysisContext context)
+        {
+            SyntaxNode root = context.Tree.GetCompilationUnitRoot(context.CancellationToken);
+
+            foreach (var node in root.DescendantTrivia())
+            {
+                string comment;
+
+                switch (node.Kind())
+                {
+                    case SyntaxKind.SingleLineCommentTrivia:
+
+                        comment = node.ToString().TrimStart(TrimChars);
+                        AnalyzeComment(comment, node.GetLocation(), context);
+                        break;
+
+                    case SyntaxKind.SingleLineDocumentationCommentTrivia:
+                    case SyntaxKind.MultiLineCommentTrivia:
+
+                        comment = node.ToString();
+
+                        var offset = node.SpanStart;
+
+                        foreach (var commentLine in comment.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
+                        {
+                            var trimmedLine = commentLine.TrimStart(TrimChars);
+                            var trimLength = commentLine.Length - trimmedLine.Length;
+                            AnalyzeComment(trimmedLine, node.GetLocation(), context, offset + trimLength);
+                            offset = offset + commentLine.Length + Environment.NewLine.Length;
+                        }
+
+                        break;
+                }
+            }
+        }
+
+        private static void AnalyzeComment(string comment, Location location, SyntaxTreeAnalysisContext context, int startOffset = -1)
+        {
+            // order is important else the FIX*ME rule triggers for FIX*ME!
+            var _ =
+            LookupTerm(comment, location, context, "FIXME!", ErrOnFixmeRule, startOffset) ||
+            LookupTerm(comment, location, context, "FIXME", WarnOnFixmeRule, startOffset);
+        }
+
+        private static bool LookupTerm(string comment, Location location, SyntaxTreeAnalysisContext context, string term, DiagnosticDescriptor descriptor, int startOffset = -1)
+        {
+            if (!comment.StartsWith(term, StringComparison.OrdinalIgnoreCase)) 
+                return false;
+
+            var displayComment = comment.Substring(term.Length).TrimStart(' ', ':');
+            var displayOffset = comment.IndexOf(term, StringComparison.OrdinalIgnoreCase);
+            var locationToUse = location;
+
+            if (startOffset >= 0)
+            {
+                locationToUse = Location.Create(
+                    location.SourceTree,
+                    new TextSpan(startOffset + displayOffset, comment.Length - displayOffset));
+            }
+
+            context.ReportDiagnostic(Diagnostic.Create(descriptor, locationToUse, displayComment));
+            return true;
+        }
+    }
+}
